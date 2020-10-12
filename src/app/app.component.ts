2d030aa652123bf2d874f8d5c66c302069895cc2
badawi98 <badawi1998@icloud.com>
2020-10-12 17:53:53 +0300

    Add Navigation to Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Learning-Project';
+  loadedFeature = 'recipe';
+  onNavigate(feature : string){
+    this.loadedFeature = feature;
+  }
 }
